5616b14f0a424baea74e808f537d41b0fb4d652c
Patrick Trester <patricktrester@Patricks-MacBook-Pro.local>
2018-07-03 22:29:56 -0700

    Removed counter

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const screenshot = require('screenshot-desktop')
+const { exec } = require('child_process')
+const { readFileSync } = require('fs')
 const resizeImg = require('resize-img')
 const io = require('socket.io-client')
 const serverPass = process.argv[2]
 const serverURI = process.argv[3] || 'http://localhost'
-const refreshRate = parseInt(process.argv[4]) || 200
-const resWidth = parseInt(process.argv[5]) || 1024
-const resHeight = parseInt(process.argv[6]) || 768
+const resWidth = parseInt(process.argv[4]) || 1366
+const resHeight = parseInt(process.argv[5]) || 758
 
 var socket = io(serverURI)
 socket.emit('login', serverPass)
@@ -21,21 +21,24 @@ socket.on('login', r => {
 })
 
 function sendImage () {
-  screenshot.listDisplays().then((displays) => {
-    screenshot({ screen: displays[displays.length - 1].id })
-      .then((img) => {
-        resizeImg(img, {width: resWidth, height: resHeight})
-          .then(buffer => {
-            socket.emit('cast', buffer)
-            setTimeout(sendImage, refreshRate)
-          })
-          .catch((err) => {
-            console.log(err)
-            setTimeout(sendImage, refreshRate)
-          })
-      })
-  }).catch((err) => {
-    console.log(err)
-    setTimeout(sendImage, refreshRate)
+  exec('screencapture -m -t jpg -x /tmp/screenshot.jpg', (err, stdout, stderr) => {
+    if (err) {
+      console.log(err)
+      sendImage()
+    } else if (stderr) {
+      console.log(stderr)
+      sendImage()
+    } else {
+      let img = readFileSync('/tmp/screenshot.jpg')
+      resizeImg(img, {width: resWidth, height: resHeight})
+        .then(buffer => {
+          socket.emit('cast', buffer)
+          sendImage()
+        })
+        .catch(err => {
+          console.log(err)
+          sendImage()
+        })
+    }
   })
 }
